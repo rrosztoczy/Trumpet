1de6d4716179a02e0d151b792ef295021e18741d
rrosztoczy <rrosztoczy@gmail.com>
2019-05-03 12:36:11 -0400

    got form to shwo

--- src/Components/EditTrumpetForm.js
+++ src/Components/EditTrumpetForm.js
@@ -6,6 +6,8 @@ const options = [
     { key: 'l', text: 'Lie', name: "trumpet_type", value: 'Lie' },
     { key: 'n', text: 'No Sources', name: "trumpet_type", value: 'No Sources' }
   ]
+
+  
 export default class EditTrumpetForm extends React.Component {
 
     // console.log("object", trumpet)
@@ -21,6 +23,15 @@ export default class EditTrumpetForm extends React.Component {
     //     }
     // }
 
+
+    state = {
+      summary: this.props.trumpet.summary,
+      url: this.props.trumpet.url,
+      root_url: this.props.trumpet.root_url,
+      trumpet_type: this.props.trumpet.trumpet_type,
+      content: this.props.trumpet.content
+    }
+
     handleChange = (event) => this.setState({[event.target.name]: event.target.value}, () => console.log("State", this.state))
     handleSelect = (event) => {
         event.persist()
@@ -30,17 +41,19 @@ export default class EditTrumpetForm extends React.Component {
     // Fully control form with state
   
     render() {
-      const { value } = this.state
+      const { state } = this.state
+      console.log("edit state", this.state)
       return (
-        <Form onSubmit={(event, newTrumpet) => this.props.handleSubmit(event, this.state)}>
+        <Form >
+         {/* <Form onSubmit={(event, newTrumpet) => this.props.handleSubmit(event, this.state)}> */}
           <Form.Group widths='equal'>
-            <Form.Input onChange={this.handleChange} fluid label='Summary' placeholder='Summary' name="summary" value={this.state.summary ? this.state.summary : null} />
+            <Form.Input onChange={this.handleChange} fluid label='Summary' placeholder='Summary' name="summary" value={this.state.summary} />
             {/* Figure out selection value */}
-            <Form.Select onChange={this.handleSelect} fluid label='Type' options={options} placeholder='Type' name="trumpet_type" value={this.state.trumpet_type ? this.state.trumpet_type : null} />
+            <Form.Select onChange={this.handleSelect} fluid label='Type' options={options} placeholder='Type' name="trumpet_type" value={this.state.trumpet_type} />
           </Form.Group>
           <Form.Group widths='equal'>
-          <Form.Input onChange={this.handleChange} fluid label='Website URL' placeholder='Website URL' name="url" value={this.state.url ? this.state.url : null} />
-            <Form.Input onChange={this.handleChange} fluid label='Root Url' placeholder='Root Url' name="root_url" value={this.state.root_url ? this.state.root_url : null} />
+          <Form.Input onChange={this.handleChange} fluid label='Website URL' placeholder='Website URL' name="url" value={this.state.url} />
+            <Form.Input onChange={this.handleChange} fluid label='Root Url' placeholder='Root Url' name="root_url" value={this.state.root_url} />
           </Form.Group>
           <Form.TextArea onChange={this.handleChange} label='Content' placeholder='Tell us more about you...' name="content" value={this.state.content} />
           {/* <Form.Checkbox label='I agree to the Terms and Conditions' /> */}
