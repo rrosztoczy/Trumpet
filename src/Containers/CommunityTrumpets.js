e0624299fac53fdc9c286ae9797e99528fcfadde
rrosztoczy <rrosztoczy@gmail.com>
2019-05-07 15:14:18 -0400

    added mock charts

--- src/Containers/CommunityTrumpets.js
+++ src/Containers/CommunityTrumpets.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import Trumpets from './Trumpets'
+import { Grid } from 'semantic-ui-react'
+import TrumpetLineChart from '../Components/TrumpetLineChart'
+import TrumpetDoughnut from '../Components/TrumpetDoughnut'
 
 export default class CommunityTrumpets extends Component {
 
     render() {
-      return <Trumpets noEdit="noEdit" trumpets={this.props.trumpets} onReactionClick={this.props.onReactionClick}></Trumpets>
-    }
+      return (
+        <Grid celled>
+        <Grid.Column width={8}>
+          <header>Trumpets Your Have Created</header>
+          <Trumpets noEdit="noEdit" trumpets={this.props.trumpets} onReactionClick={this.props.onReactionClick}></Trumpets>
+        </Grid.Column>
+        <Grid.Column width={8}>
+        <Grid.Row>
+        <header>Holder for overall breakdown</header>
+        <TrumpetDoughnut />
+        </Grid.Row>
+        <Grid.Row>
+        <header>Holder for trending chart</header>
+        <TrumpetLineChart />
+        </Grid.Row>
+        </Grid.Column>
+        </Grid>
+      )}
 }
